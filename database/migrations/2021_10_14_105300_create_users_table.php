2dbc3d776747f152532427d67cf3d2b5bf7613bc
Dimanok1989 <kolgaev@gmail.com>
2021-10-28 13:55:06 +0300

    Add users table migration

---
+++ database/migrations/2021_10_14_105300_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('pin')->nullable()->comment('Персональный идентификационный номер')->index();
+            $table->string('old_pin', 50)->nullable()->comment('Идентификатор из старой ЦРМ');
+            $table->string('login', 250)->nullable()->comment('Логин для авторизации')->index();
+            $table->integer('callcenter_id')->nullable()->comment('Идентификатор колл-центра');
+            $table->integer('callcenter_sector_id')->nullable()->comment('Идентификатор сектора колл-центра');
+            $table->string('surname', 250)->nullable()->comment('Фамилия');
+            $table->string('name', 250)->nullable()->comment('Имя');
+            $table->string('patronymic', 250)->nullable()->comment('Отчество');
+            $table->string('password', 250)->nullable()->comment('Хэш пароля');
+            $table->string('telegram_id', 100)->nullable()->comment('Идентификатор Телеграм чата');
+            $table->string('auth_type', 50)->nullable()->comment('Тип авторизации');
+            $table->timestamps();
+            $table->softDeletes()->comment("Время удаления или блокировки");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
