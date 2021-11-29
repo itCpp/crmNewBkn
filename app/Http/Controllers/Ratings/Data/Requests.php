8304671ab453b374650336433a35d10685f068fb
Dimanok1989 <kolgaev@gmail.com>
2021-11-29 18:44:18 +0300

    Расчет заявок по каждому дню раздельно

--- app/Http/Controllers/Ratings/Data/Requests.php
+++ app/Http/Controllers/Ratings/Data/Requests.php
@@ -13,22 +13,59 @@ trait Requests
      */
     public function getRequests()
     {
-        $this->data->requests = RequestsRow::selectRaw('COUNT(*) as count, pin')
-            ->whereDate('s', "202")
+        $rows = RequestsRow::select(
+            'pin',
+            'created_at',
+            'check_moscow',
+            'region'
+        )
+            ->whereNotNull('pin')
             ->whereBetween('created_at', [
                 $this->dates->start . " 00:00:00",
                 $this->dates->stop . " 23:59:59"
             ])
-            ->whereNotNull('pin')
-            ->groupBy('pin')
-            ->get()
-            ->map(function ($row) {
+            ->get();
 
-                if (!in_array($row->pin, $this->data->pins))
-                    $this->data->pins[] = $row->pin;
+        $requests = [];
 
-                return (object) $row->toArray();
-            });
+        foreach ($rows as $row) {
+
+            $date = date("Y-m-d", strtotime($row->created_at));
+
+            if (!in_array($row->pin, $this->data->pins))
+                $this->data->pins[] = $row->pin;
+
+            if (empty($requests[$row->pin])) {
+                $requests[$row->pin] = [
+                    'all' => 0,
+                    'moscow' => 0,
+                    'pin' => $row->pin,
+                    'dates' => [],
+                ];
+            }
+
+            if (empty($requests[$row->pin]['dates'][$date])) {
+                $requests[$row->pin]['dates'][$date] = [
+                    'all' => 0,
+                    'moscow' => 0,
+                ];
+            }
+
+            $requests[$row->pin]['all']++;
+            $requests[$row->pin]['dates'][$date]['all']++;
+
+            if (
+                $row->check_moscow == 'moscow'
+                or $row->region == 'Неизвестно'
+                or $row->region == ''
+                or $row->region == NULL
+            ) {
+                $requests[$row->pin]['moscow']++;
+                $requests[$row->pin]['dates'][$date]['moscow']++;
+            }
+        }
+
+        $this->data->requests = $requests;
 
         return $this;
     }
