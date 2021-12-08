f7a5248d0a87ad5bd3d3fb3bed86db3c284b8585
Dimanok1989 <kolgaev@gmail.com>
2021-12-08 11:40:06 +0300

    Данные списка чатов

--- app/Http/Controllers/Chats/StartChat.php
+++ app/Http/Controllers/Chats/StartChat.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Chats;
 use App\Http\Controllers\Controller;
 use App\Models\ChatIdUser;
 use App\Models\ChatMessage;
+use App\Models\CrmMka\CrmUser as User;
 use Illuminate\Http\Request;
 
 class StartChat extends Controller
@@ -38,14 +39,61 @@ class StartChat extends Controller
      */
     public function getChatsRooms()
     {
-        ChatIdUser::where('user_id', $this->request->user()->pin)
-        ->get()
-        ->map(function ($row) use (&$rooms) {
-            $rooms[] = $row->chat_id;
+        $chats = ChatIdUser::where('user_id', $this->request->user()->pin)
+            ->get()
+            ->map(function ($row) {
+                return $row->chat_id;
+            })
+            ->toArray();
 
-            return $row;
+        $rooms = $this->getChatsRoomsData($chats);
+
+        usort($rooms, function ($a, $b) {
+            return (int) $a['sort'] > (int) $b['sort'];
         });
 
+        return $rooms;
+    }
+
+    /**
+     * Поиск наименований чатов
+     * 
+     * @param array $chats
+     * @return array
+     */
+    public function getChatsRoomsData($chats = [])
+    {
+        User::whereIn('id', $chats)
+            ->get()
+            ->map(function ($row) use (&$rooms) {
+
+                $message = $this->findLastMessage($row->id);
+
+                $rooms[] = [
+                    'id' => $row->id,
+                    'name' => $row->fullName,
+                    'pin' => $row->pin,
+                    'message' => $message,
+                    'sort' => strtotime($message['created_at'] ?? null),
+                ];
+
+                return $row;
+            });
+        
         return $rooms ?? [];
+    }
+
+    /**
+     * Поиск последнего сообщения
+     * 
+     * @param int $chat_id
+     * @return null|array
+     */
+    public function findLastMessage($chat_id)
+    {
+        if (!$message = ChatMessage::where('chat_id', $chat_id)->orderBy('id', "DESC")->first())
+            return null;
+
+        return $message->toArray();
     }
 }
