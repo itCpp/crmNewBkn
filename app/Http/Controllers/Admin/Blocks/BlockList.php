0f3006045cb8d2f783d21081f7d2dfdb22e92fed
Dimanok1989 <kolgaev@gmail.com>
2022-01-31 13:41:33 +0300

    вывод данных для страницы управдения блокировок

--- app/Http/Controllers/Admin/Blocks/BlockList.php
+++ app/Http/Controllers/Admin/Blocks/BlockList.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin\Blocks;
 
 use App\Http\Controllers\Controller;
 use App\Models\Company\BlockHost;
+use App\Models\Company\StatVisit;
+use App\Models\IpInfo;
 use Illuminate\Http\Request;
 
 class BlockList extends Controller
@@ -16,17 +18,56 @@ class BlockList extends Controller
      */
     public static function get(Request $request)
     {
-        $rows = new BlockHost;
+        $data = BlockHost::when((bool) $request->search, function ($query) use ($request) {
+            $query->where('host', 'LIKE', "%{$request->search}%")
+                ->orderBy('host');
+        })
+            ->when((bool) $request->serach === false, function ($query) {
+                $query->orderBy('id', "DESC");
+            })
+            ->when($request->ipv6 and !$request->ipv4, function ($query) {
+                $query->where('host', 'like', '%:%');
+            })
+            ->when(!$request->ipv6 and $request->ipv4, function ($query) {
+                $query->where('host', 'not like', '%:%');
+            })
+            ->paginate(50);
 
-        if ($request->search)
-            $rows = $rows->where('host', 'LIKE', "%{$request->search}%")->orderBy('host');
-        else
-            $rows = $rows->orderBy('id', "DESC");
+        $rows = [];
+        $ips = [];
 
-        $rows = $rows->limit(35)->get();
+        $data->each(function ($row) use (&$rows, &$ips) {
+
+            $ips[] = $row->host;
+
+            $row->info = IpInfo::select('country_code', 'region_name', 'city')
+                ->where('ip', $row->host)
+                ->first();
+
+            $rows[$row->host] = $row->toArray();
+        });
+
+        IpInfo::select('ip', 'country_code', 'region_name', 'city')
+            ->whereIn('ip', $ips)
+            ->get()
+            ->each(function ($row) use (&$rows) {
+                $rows[$row->ip]['info'] = $row;
+            });
+
+        StatVisit::select('ip', 'host')
+            ->whereIn('ip', $ips)
+            ->distinct()
+            ->get()
+            ->each(function ($row) use (&$rows) {
+                $rows[$row->ip]['hostname'] = $row->host;
+            });
 
         return [
-            'rows' => $rows,
+            'rows' => collect($rows)->values()->all(),
+            'pages' => $data->lastPage(),
+            'next' => $data->currentPage() + 1,
+            'page' => $data->currentPage(),
+            'total' => $data->total(),
         ];
     }
 }
