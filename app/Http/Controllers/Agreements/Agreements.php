11af2fd906ce02da6b170723ce39da52d87c6f20
Dimanok1989 <kolgaev@gmail.com>
2022-02-18 18:05:15 +0300

    Рефакторинг контроллера

--- app/Http/Controllers/Agreements/Agreements.php
+++ app/Http/Controllers/Agreements/Agreements.php
@@ -8,6 +8,7 @@ use App\Models\Base\CrmAgreementComment;
 use App\Models\Base\CrmKassa;
 use App\Models\Base\Office;
 use App\Models\Base\Personal;
+use App\Models\RequestsRow;
 use Illuminate\Http\Request;
 
 class Agreements extends Controller
@@ -75,151 +76,58 @@ class Agreements extends Controller
     public function serialize(CrmAgreement $row)
     {
         // Список персональных номеров ЮПП
-        foreach (explode("/", $row->oristFio ?? "") as $oristFio) {
-
-            if (trim($oristFio) == "")
-                continue;
-
-            $oristFioArr[] = $this->getPersonalData(trim($oristFio));
-        }
-
-        $row->oristFioArr = $oristFioArr ?: [];
-
-        // Объект расходов
-        $row->predstavRashodJson = json_decode($row->predstavRashodJson) ?: [];
+        $row->oristFioArr = $this->getLawyerList($row->oristFio);
 
         // Добавление иконки офиса
         $row->icon = $this->getOfficeIcon($row->company);
 
         // Определение цвета строки
-        $row->color = 'status-no';
-        switch ($row->colStatus) {
-            case 0:
-                $row->color = 'status-neobr';
-                break;
-            case 1:
-                $row->color = 'status-good';
-                break;
-            case 2:
-                $row->color = 'status-check';
-                break;
-            case 3:
-                $row->color = 'nstatus-bad';
-                break;
-            case 4:
-                $row->color = 'status-nocall';
-                break;
-        }
+        $row->color = $this->getRowColor($row->colStatus);
 
         // Идентификаторы заявок
         $row->unicIdClient = (int) $row->unicIdClient;
 
-        // Номера телефонов клиента
-        $phones = json_decode($row->phones, true) ?: [];
-        $phoneslist = [];
+        // Список номеров телефона клиента
+        $row->phones = $this->getPhones($row);
 
-        // Поиск номеров из таблицы клиента
-        foreach ($phones as $phone) {
+        $row->avans = (int) $row->avans;
+        $row->summa = (int) $row->summa;
+        $row->ostatok = (int) $row->ostatok;
+        $row->predstavRashod = $this->getPredstavRashodSumm($row->nomerDogovora);
 
-            $phone = $this->checkPhone($phone, 3);
+        // Объект расходов
+        $row->predstavRashodJson = json_decode($row->predstavRashodJson) ?: [];
 
-            if ($phone and !in_array($phone, $phoneslist))
-                $phoneslist[] = $phone;
-        }
+        $this->getComments($row);
 
-        // Поиск номеров из таблицы договора
-        $phones = explode(" ", $row->phone);
-
-        foreach ($phones as $phone) {
-
-            $phone = $this->checkPhone($phone, 3);
-
-            if ($phone and !in_array($phone, $phoneslist))
-                $phoneslist[] = $phone;
-        }
-
-        // Список номеров клиента
-        $row->phones = $phoneslist;
-
-        // Получение всех данных по договору
-        $comments_rows = CrmAgreementComment::where('del', 0)
-            ->where('idAgreement', $row->id)
-            ->whereIn('type', $this->comment_types)
-            ->orderBy('date', 'DESC')
-            ->get();
-
-        $comments = []; // Дополнительные данные
-        $predstavs = []; // Писок представителей
-        $odIspolnitel = null; // Текущий исполнитель
-        $predmet = null; // Предмет договора
-
-        foreach ($this->comment_types as $type) {
-            $comments[$type] = [];
-        }
-
-        foreach ($comments_rows as $comment) {
-
-            $comment->text = htmlspecialchars_decode($comment->text);
-
-            // Обработка данных акта
-            if ($comment->type == "act") {
-
-                if ($comment->text = json_decode($comment->text)) {
-
-                    switch ($comment->text->type) {
-                        case '0':
-                            $comment->text->type = 'Акт';
-                            break;
-                        case '1':
-                            $comment->text->type = 'Промакт';
-                            break;
-                        case '2':
-                            $comment->text->type = 'Акт с объяснительной';
-                            break;
-                    }
-                }
-            }
-
-            if ($comment->type == "predmetDogovora") {
-
-                if (!$predmet)
-                    $predmet = $comment->text;
-            }
-
-            // Добавление представителей
-            if ($comment->type == "odIspolnitel") {
-
-                $comment->textPin = $this->getPersonalData($comment->text);
-
-                if (!$odIspolnitel)
-                    $odIspolnitel = $comment->textPin;
-
-                $predstavs[] = $comment->textPin;
-            }
-
-            // Запись данных
-            $comments[$comment->type][] = (object) [
-                'id' => $comment->id,
-                'created_at' => $comment->date,
-                'text' => $comment->text,
-                'textPin' => $comment->textPin,
-                'author' => $this->getPersonalData($comment->pin),
-            ];
-        }
-
-        $row->comments = $comments;
-
-        $row->odIspolnitel = $odIspolnitel;
-
-        if ($predmet)
-            $row->predmetDogovora = $predmet;
-
-        return array_merge($row->toArray(), [
-            'avans' => (int) $row->avans,
-            'summa' => (int) $row->summa,
-            'ostatok' => (int) $row->ostatok,
-            'predstavRashod' => $this->getPredstavRashodSumm($row->nomerDogovora),
-        ]);
+        return collect($row->toArray())->only([
+            'FullNameClient',
+            'avans',
+            'colStatus',
+            'collDate',
+            'color',
+            'coming_date',
+            'coming_time',
+            'comment',
+            'commentOkk',
+            'comments',
+            'date',
+            'icon',
+            'id',
+            'nomerDogovora',
+            'odIspolnitel',
+            'oristFioArr',
+            'ostatok',
+            'phones',
+            'predmetDogovora',
+            'predstavRashod',
+            'predstavRashodJson',
+            'rashodPoDogovory',
+            'status',
+            'summa',
+            'tematika',
+            'unicIdClient',
+        ])->all();
     }
 
     /**
@@ -274,5 +182,222 @@ class Agreements extends Controller
             ->where('nomerDogovora', $number)
             ->where('predstavRashod', '!=', "")
             ->sum('predstavRashod');
+    }
+
+    /**
+     * Определение цвета строки
+     * 
+     * @param string|int $status
+     * @return string
+     */
+    public function getRowColor($status)
+    {
+        if ($status == 0)
+            return "status-neobr";
+
+        if ($status == 1)
+            return "status-good";
+
+        if ($status == 2)
+            return "status-check";
+
+        if ($status == 3)
+            return "nstatus-bad";
+
+        if ($status == 4)
+            return "status-nocall";
+
+        return "status-no";
+    }
+
+    /**
+     * Определение списка ЮПП
+     * 
+     * @param string $upp
+     * @return array
+     */
+    public function getLawyerList($upp)
+    {
+        foreach (explode("/", $upp ?? "") as $row) {
+
+            if (trim($row) == "")
+                continue;
+
+            $list[] = $this->getPersonalData(trim($row));
+        }
+
+        return $list ?? [];
+    }
+
+    /**
+     * Поиск номеров телефона клинта
+     * 
+     * @param object $row
+     * @return array
+     */
+    public function getPhones($row)
+    {
+        if ($row->unicIdClient)
+            return $this->getPhonesFromRequest($row->unicIdClient);
+
+        $phones = json_decode($row->phones, true) ?: [];
+        $list = [];
+
+        // Поиск номеров из таблицы клиента
+        foreach ($phones as $phone) {
+
+            $phone = $this->checkPhone($phone, 3);
+
+            if ($phone and !in_array($phone, $list))
+                $list[] = $phone;
+        }
+
+        // Поиск номеров из таблицы договора
+        $phones = explode(" ", $row->phone);
+
+        foreach ($phones as $phone) {
+
+            $phone = $this->checkPhone($phone, 3);
+
+            if ($phone and !in_array($phone, $list))
+                $list[] = $phone;
+        }
+
+        $phones = [];
+
+        foreach ($list ?? [] as $key => $phone) {
+            $phones[] = $this->serializePhoneRow($phone, "+{$row->id}d{$key}");
+        }
+
+        return $phones;
+    }
+
+    /**
+     * Номера телефона из заявки
+     * 
+     * @param int $id
+     * @return array
+     */
+    public function getPhonesFromRequest($id)
+    {
+        if (!$row = RequestsRow::find($id))
+            return [];
+
+        foreach ($row->clients as $client) {
+
+            $phone = $this->decrypt($client->phone);
+
+            $phones[] = $this->serializePhoneRow($phone, "+{$id}s{$client->id}");
+        }
+
+        return $phones ?? [];
+    }
+
+    /**
+     * Формирование строки с номером телефона
+     * 
+     * @param string $phone
+     * @param string id
+     * @return array
+     */
+    public static function serializePhoneRow($phone, $id)
+    {
+        $show = request()->user()->can('clients_show_phone');
+        $type = $show ? parent::KEY_PHONE_SHOW : parent::KEY_PHONE_HIDDEN;
+
+        return [
+            'number' => $show ? parent::checkPhone($phone, 3) : $id,
+            'phone' => parent::checkPhone($phone, $type),
+        ];
+    }
+
+    /**
+     * Вывод всех комментариев по договору
+     * 
+     * @param  \App\Models\Base\CrmAgreement  $row
+     * @return \App\Models\Base\CrmAgreement
+     */
+    public function getComments(&$row)
+    {
+        $rows = CrmAgreementComment::where('del', 0)
+            ->where('idAgreement', $row->id)
+            ->whereIn('type', $this->comment_types)
+            ->orderBy('date', 'DESC')
+            ->get();
+
+        $comments = []; // Все комментарии
+        $predstavs = []; // Список всех представителей
+
+        $odIspolnitel = null; // Текущий исполнитель
+        $predmet = null; // Предмет договора
+
+        /** Создание пустых контейнеров */
+        foreach ($this->comment_types as $type) {
+            $comments[$type] = [];
+        }
+
+        foreach ($rows as $comment) {
+
+            if ($comment->type == "act")
+                $comment->text = $this->getActData($comment->text);
+            else
+                $comment->text = htmlspecialchars_decode($comment->text);
+
+            if ($comment->type == "predmetDogovora") {
+
+                if (!$predmet)
+                    $predmet = $comment->text;
+            }
+
+            // Добавление представителей
+            if ($comment->type == "odIspolnitel") {
+
+                $comment->textPin = $this->getPersonalData($comment->text);
+
+                if (!$odIspolnitel)
+                    $odIspolnitel = $comment->textPin;
+
+                $predstavs[] = $comment->textPin;
+            }
+
+            // Запись данных
+            $comments[$comment->type][] = (object) [
+                'id' => $comment->id,
+                'created_at' => $comment->date,
+                'text' => $comment->text,
+                'textPin' => $comment->textPin,
+                'author' => $this->getPersonalData($comment->pin),
+            ];
+        }
+
+        $row->comments = $comments;
+
+        $row->odIspolnitel = $odIspolnitel;
+
+        if ($predmet)
+            $row->predmetDogovora = $predmet;
+
+        return $row;
+    }
+
+    /**
+     * Данные акта
+     * 
+     * @param string $data JSON строка
+     * @return array|string
+     */
+    public function getActData($data)
+    {
+        if ($response = json_decode($data, true)) {
+
+            if ($response['type'] == '0')
+                $response['type'] = 'Акт';
+            else if ($response['type'] == '1')
+                $response['type'] = 'Промакт';
+            else if ($response['type'] == '2')
+                $response['type'] = 'Акт с объяснительной';
+        }
+
+        return $response ?: $data;
     }
 }
