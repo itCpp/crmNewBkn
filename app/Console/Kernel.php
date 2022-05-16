b8a9a528fb22ca88727439d6edff2c20dc6944eb
Dimanok1989 <kolgaev@gmail.com>
2022-05-16 17:36:24 +0300

    Сохранение результата работы планировщика

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,28 +26,45 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
+        $path = "logs/cron/result/" . date("Y/m/d/H");
+        $file_name = date("YmdHis");
+
         /** Проверка входящих СМС на шлюзах */
         $schedule->command('sms:incomings')
             ->everyMinute()
             ->between('8:30', '22:00')
+            ->sendOutputTo(storage_path("{$path}/{$file_name}_sms_incomings.log"))
             ->runInBackground();
 
         /** Проверка очереди заявок */
         $schedule->command('requests:getfromsite', ['--while', '--sleep' => 10])
             ->everyMinute()
+            ->sendOutputTo(storage_path("{$path}/{$file_name}_requests_getfromsite.log"))
             ->runInBackground();
 
         /** Перешифровка данных событий с использованием внутреннего ключа шифрования */
-        $schedule->command('events:recrypt')->everyMinute()->runInBackground();
+        $schedule->command('events:recrypt')
+            ->everyMinute()
+            ->sendOutputTo(storage_path("{$path}/{$file_name}_events_recrypt.log"))
+            ->runInBackground();
 
         /** Завершает все активные сессии */
-        $schedule->command('users:endsessions')->dailyAt("20:30")->runInBackground();
+        $schedule->command('users:endsessions')
+            ->dailyAt("20:30")
+            ->sendOutputTo(storage_path("{$path}/{$file_name}_users_endsessions.log"))
+            ->runInBackground();
 
         /** Запись истории счетчика заявок */
-        $schedule->command('story:counter')->dailyAt("23:59")->runInBackground();
+        $schedule->command('story:counter')
+            ->dailyAt("23:59")
+            ->sendOutputTo(storage_path("{$path}/{$file_name}_story_counter.log"))
+            ->runInBackground();
 
         /** Запись истории рейтинга колл-центра */
-        $schedule->command('rating:write')->dailyAt("23:59")->runInBackground();
+        $schedule->command('rating:write')
+            ->dailyAt("23:59")
+            ->sendOutputTo(storage_path("{$path}/{$file_name}_rating_write.log"))
+            ->runInBackground();
     }
 
     /**
